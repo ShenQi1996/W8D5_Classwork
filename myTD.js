2a4f2b11b87ca71d44d43c89387930a80d459ccf
aturi <angelaturi99@gmail.com>
2021-04-16 17:33:36 -0400

    Finish debounce

--- myTD.js
+++ myTD.js
@@ -12,29 +12,29 @@ Function.prototype.myThrottle = function (interval1) {
   };
 };
 
-class Neuron {
-  fire() {
-    console.log("Firing!");
-  }
-}
+// class Neuron {
+//   fire() {
+//     console.log("Firing!");
+//   }
+// }
 
-const neuron = new Neuron();
-// When we create a new Neuron,
-// we can call #fire as frequently as we want
+// const neuron = new Neuron();
+// // When we create a new Neuron,
+// // we can call #fire as frequently as we want
 
-// The following code will try to #fire the neuron every 10ms. Try it in the console:
-const interval = setInterval(() => {
-  neuron.fire();
-}, 10);
+// // The following code will try to #fire the neuron every 10ms. Try it in the console:
+// const interval = setInterval(() => {
+//   neuron.fire();
+// }, 10);
 
-// You can use clearInterval to stop the firing:
-clearInterval(interval);
+// // You can use clearInterval to stop the firing:
+// clearInterval(interval);
 
-// // Using Function#myThrottle, we should be able to throttle
-// // the #fire function of our neuron so that it can only fire
-// // once every 500ms:
+// // // Using Function#myThrottle, we should be able to throttle
+// // // the #fire function of our neuron so that it can only fire
+// // // once every 500ms:
 
-neuron.fire = neuron.fire.myThrottle(500);
+// neuron.fire = neuron.fire.myThrottle(500);
 
 // const interval = setInterval(() => {
 //   neuron.fire();
@@ -56,3 +56,70 @@ neuron.fire = neuron.fire.myThrottle(500);
 //     console.log("Firing!");
 //   }
 // }
+
+
+Function.prototype.myDebounce = function(interval) {
+  let timeout;
+
+  return (...args) => {
+
+    const func = () => {
+      timeout = null;
+      this(...args);
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(func, interval);
+  }
+  // let that = this;
+
+  // return function anonFunc() {
+  //   // setTimeout(that, interval);
+  //   console.log(that, this);
+  // }
+
+
+}
+
+// let newFunc = new Function ();
+// console.log(newFunc.myDebounce(1000));
+
+class SearchBar {
+  constructor() {
+    this.query = "";
+
+    this.type = this.type.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  type(letter) {
+    this.query += letter;
+    this.search();
+  }
+
+  search() {
+    console.log(`searching for ${this.query}`);
+  }
+}
+
+const searchBar = new SearchBar();
+
+searchBar.search = searchBar.search.myDebounce(500);
+
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+queryForHelloWorld();
+
+// searchBar.search = searchBar.search.myDebounce(500);
